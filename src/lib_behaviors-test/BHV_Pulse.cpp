c5008d7924ee515cbc0b80af3b98ea71bd421597
mschwartzman2 <mschwartzman@sea-machines.com>
2025-04-08 12:29:31 -0400

    feat: adds pulse behavior

--- src/lib_behaviors-test/BHV_Pulse.cpp
+++ src/lib_behaviors-test/BHV_Pulse.cpp
@@ -10,6 +10,7 @@
 #include "MBUtils.h"
 #include "BuildUtils.h"
 #include "BHV_Pulse.h"
+#include "XYRangePulse.h"
 
 using namespace std;
 
@@ -19,6 +20,17 @@ using namespace std;
 BHV_Pulse::BHV_Pulse(IvPDomain domain) :
   IvPBehavior(domain)
 {
+  m_position_x = 0;
+  m_position_y = 0;
+  m_arrival_time = 0;
+  m_current_time = 0;
+  m_waypoint_index = -1;
+  
+  m_pulse_duration = 4;
+  m_pulse_radius = 20;
+  m_pulse_needed = false;
+  m_pulse_delay  = 5;
+
   // Provide a default behavior name
   IvPBehavior::setParam("name", "defaultname");
 
@@ -27,6 +39,7 @@ BHV_Pulse::BHV_Pulse(IvPDomain domain) :
 
   // Add any variables this behavior needs to subscribe for
   addInfoVars("NAV_X, NAV_Y");
+  addInfoVars("WPT_INDEX");
 }
 
 //---------------------------------------------------------------
@@ -40,12 +53,19 @@ bool BHV_Pulse::setParam(string param, string val)
   // Get the numerical value of the param argument for convenience once
   double double_val = atof(val.c_str());
   
-  if((param == "foo") && isNumber(val)) {
+  if((param == "pulse_radius") && isNumber(val)) {
     // Set local member variables here
+    m_pulse_radius = double_val;
     return(true);
   }
-  else if (param == "bar") {
-    // return(setBooleanOnString(m_my_bool, val));
+  else if((param == "pulse_duration") && isNumber(val)) {
+    // Set local member variables here
+    m_pulse_duration = double_val;
+    return(true);
+  }
+  else if ((param == "pulse_delay") && isNumber(val)) {
+    m_pulse_delay = double_val;
+    return(true);
   }
 
   // If not handled above, then just return false;
@@ -119,7 +139,44 @@ IvPFunction* BHV_Pulse::onRunState()
   // Part 1: Build the IvP function
   IvPFunction *ipf = 0;
 
+  bool okx, oky, oki, transitioned_points;
 
+  m_position_x = getBufferDoubleVal("NAV_X", okx);
+  m_position_y = getBufferDoubleVal("NAV_Y", oky);
+  m_current_time = getBufferCurrTime();
+  int last_index = m_waypoint_index;
+  postMessage("CURRI", m_waypoint_index);
+  postMessage("LASTI", last_index);
+  postMessage("ARRIVAL_TIME", m_arrival_time);
+  postMessage("CURRENT_TIME", m_current_time);
+  m_waypoint_index = getBufferDoubleVal("WPT_INDEX", oki);
+
+  if(!okx || !oky) {
+    postWMessage("No ownship X/Y info in info_buffer.");
+    return(0);
+  }
+  if(!oki) {
+    postWMessage("No wpt index info found in buffer!");
+    return(0);
+  }
+
+  if (m_waypoint_index != last_index){
+    transitioned_points = true;
+    
+  }
+
+  if (transitioned_points){
+    m_arrival_time = getBufferCurrTime();
+    transitioned_points = false;
+    m_pulse_needed = true;
+  }
+
+  if ((m_current_time - m_arrival_time) > m_pulse_delay){
+    if (m_pulse_needed){
+      sendPulse();
+      m_pulse_needed = false;
+    }
+  }
 
   // Part N: Prior to returning the IvP function, apply the priority wt
   // Actual weight applied may be some value different than the configured
@@ -128,5 +185,26 @@ IvPFunction* BHV_Pulse::onRunState()
     ipf->setPWT(m_priority_wt);
 
   return(ipf);
+}
+
+//---------------------------------------------------------------
+// Procedure: sendPulse()
+//   Purpose: Generate the xyrangepulse object and publish it to the MOOSDB.
+
+void BHV_Pulse::sendPulse(){
+
+  XYRangePulse pulse;
+  pulse.set_x(m_position_x);
+  pulse.set_y(m_position_y);
+  pulse.set_label("bhv_pulse");
+  pulse.set_rad(m_pulse_radius);
+  pulse.set_time(m_current_time);       
+  pulse.set_color("edge", "yellow");
+  pulse.set_color("fill", "yellow");
+  pulse.set_duration(m_pulse_duration);
+
+  string spec = pulse.get_spec();
+  postMessage("VIEW_RANGE_PULSE", spec);
+
 }
 
--- src/lib_behaviors-test/BHV_Pulse.h
+++ src/lib_behaviors-test/BHV_Pulse.h
@@ -24,13 +24,25 @@ public:
   void         postConfigStatus();
   void         onRunToIdleState();
   void         onIdleToRunState();
+  void         sendPulse();
   IvPFunction* onRunState();
 
 protected: // Local Utility functions
 
 protected: // Configuration parameters
+  double m_pulse_radius;
+  double m_pulse_duration;
+  double m_pulse_delay;
 
 protected: // State variables
+  double m_position_x;
+  double m_position_y;
+  double m_current_time;
+  double m_arrival_time;
+
+  bool m_pulse_needed;
+
+  int m_waypoint_index;
 };
 
 #define IVP_EXPORT_FUNCTION
